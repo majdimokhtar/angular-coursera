9276d8b7442f800ada20ae3ac3c5f7346976c5cc
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2023-09-06 15:44:22 +0100

    assignment 5

--- assignment5/src/common/menu.service.js
+++ assignment5/src/common/menu.service.js
@@ -1,39 +1,27 @@
 (function () {
-    "use strict";
+"use strict";
 
-    angular.module('common')
-    .service('MenuService', MenuService);
+angular.module('common')
+.service('MenuService', MenuService);
 
 
-    MenuService.$inject = ['$http', 'ApiPath'];
-    function MenuService($http, ApiPath) {
-      let service = this;
-      let user = {};
+MenuService.$inject = ['$http', 'ApiPath'];
+function MenuService($http, ApiPath) {
+  var service = this;
 
-      service.getCategories = function () {
-        return $http.get(ApiPath + '/categories.json').then(function (response) {
-          return response.data;
-        });
-      };
-      service.getMenuItems = function (category) {
-        var config = {};
-        if (category) {
-          config.params = {'category': category};
-        }
+  service.getCategories = function () {
+    return $http.get(ApiPath + '/categories.json').then(function (response) {
+      return response.data;
+    });
+  };
 
-        return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
-          return response.data;
-        });
-      };
-      service.getMenuItem = function(shortName) {
-          return $http.get(ApiPath + '/menu_items/' + shortName + '.json');
-      }
-      service.setUser = function(userObj) {
-          user = userObj;
-      }
-      service.getUser = function() {
-          return user;
-      }
+
+  service.getMenuItems = function (category) {
+    return $http.get(ApiPath + '/menu_items/' + category + '.json').then(function (response) {
+      return response.data;
+    });
+  };
+
 }
 
 
--- assignment5/src/public/public.routes.js
+++ assignment5/src/public/public.routes.js
@@ -1,62 +1,58 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    angular.module('public')
+  angular.module('public')
     .config(routeConfig);
 
-    /**
-     * Configures the routes and views
-     */
-    routeConfig.$inject = ['$stateProvider'];
-    function routeConfig ($stateProvider) {
-      // Routes
-      $stateProvider
-        .state('public', {
-          abstract: true,
-          templateUrl: 'src/public/public.html'
-        })
-        .state('public.home', {
-          url: '/',
-          templateUrl: 'src/public/home/home.html'
-        })
-        .state('public.menu', {
-          url: '/menu',
-          templateUrl: 'src/public/menu/menu.html',
-          controller: 'MenuController',
-          controllerAs: 'menuCtrl',
-          resolve: {
-            menuCategories: ['MenuService', function (MenuService) {
-              return MenuService.getCategories();
-            }]
-          }
-        })
-        .state('public.menuitems', {
-          url: '/menu/{category}',
-          templateUrl: 'src/public/menu-items/menu-items.html',
-          controller: 'MenuItemsController',
-          controllerAs: 'menuItemsCtrl',
-          resolve: {
-            menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
-              return MenuService.getMenuItems($stateParams.category);
-            }]
-          }
-        })
-        .state('public.signup',{
-            url:'/signup',
-            templateUrl: 'src/public/signup/signup.html',
-            controller: 'SignUpController',
-            controllerAs: 'signup'
-        })
-        .state('public.myinfo',{
-            url:'/myinfo',
-            templateUrl: 'src/public/myinfo/myinfo.html',
-            controller: 'MyInfoController',
-            controllerAs: 'myinfo',
-            resolve: {
-                user: ['MenuService', function(MenuService) {
-                    return MenuService.getUser();
-                }]
-            }
-        });
-}
+  /**
+   * Configures the routes and views
+   */
+  routeConfig.$inject = ['$stateProvider'];
+  function routeConfig($stateProvider) {
+    // Routes
+    $stateProvider
+      .state('public', {
+        abstract: true,
+        templateUrl: 'src/public/public.html'
+      })
+      .state('public.home', {
+        url: '/',
+        templateUrl: 'src/public/home/home.html'
+      })
+      .state('myInfo', {
+        url: '/myinfo',
+        templateUrl: 'src/public/my-info/my-info-view.html',
+        controller: 'MyInfoController',
+        controllerAs: 'ctrl'
+      })
+      .state('public.menu', {
+        url: '/menu',
+        templateUrl: 'src/public/menu/menu.html',
+        controller: 'MenuController',
+        controllerAs: 'menuCtrl',
+        resolve: {
+          menuCategories: ['MenuService', function (MenuService) {
+            return MenuService.getCategories();
+          }]
+        }
+      })
+      .state('public.menusignup', {
+        url: '/menu/signup',
+        templateUrl: 'src/public/menu-signup/menu-signup.html',
+        controller: 'MenuSignupController',
+        controllerAs: 'reg',
+      })
+      .state('public.menuitems', {
+        url: '/menu/{category}',
+        templateUrl: 'src/public/menu-items/menu-items.html',
+        controller: 'MenuItemsController',
+        controllerAs: 'menuItemsCtrl',
+        resolve: {
+          menuItems: ['$stateParams', 'MenuService', function ($stateParams, MenuService) {
+            return MenuService.getMenuItems($stateParams.category);
+          }]
+        }
+      });
+
+  }
 })();
